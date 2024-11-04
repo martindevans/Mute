2d9db11e4a6fcb80a268a29ab4525bcfee6e3b03
Martin Evans <martindevans@gmail.com>
2024-11-04 02:22:12 +0000

    Added user interaction to show avatar

---
+++ Mute.Moe/Discord/Interactions/UserInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+using JetBrains.Annotations;
+using Mute.Moe.Discord.Services.Users;
+
+namespace Mute.Moe.Discord.Interactions;
+
+[UsedImplicitly]
+public class UserInfo
+    : MuteInteractionModuleBase
+{
+    public UserInfo(IHttpClientFactory http, IUserService users)
+    {
+        _users = users;
+        _http = http.CreateClient();
+    }
+
+    [UserCommand("Show Avatar")]
+    public async Task ShowAvatar(IUser user)
+    {
+        var url = user.GetAvatarUrl(ImageFormat.Png, 2048);
+        await RespondAsync(url);
+    }
+}
