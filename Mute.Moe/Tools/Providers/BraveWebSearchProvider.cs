3bb8baa1122d74f913a7210f1c5f2b197bf4c9ca
Martin Evans <martindevans@gmail.com>
2026-04-19 00:34:57 +0100

    Added a web search tool (disabled for now, need safety filtering) (#32)
    * Added a web search tool (disabled for now, need safety filtering)

--- Mute.Moe/Tools/Providers/BraveWebSearchProvider.cs
+++ Mute.Moe/Tools/Providers/BraveWebSearchProvider.cs
@@ -21,14 +21,17 @@ public class BraveWebSearchProvider
     {
         _client = client;
 
-        Tools = [ 
+        Tools =
+        [
             new AutoTool("web_search_news", true, NewsSearch),
+            //todo: new AutoTool("web_search", true, WebSearch), /* disabled for now, need to investigate filtering or white/blacklisting of URLs */
         ];
     }
 
     /// <inheritdoc />
     public IReadOnlyList<ITool> Tools { get; }
 
+    #region news
     /// <summary>
     /// Search the web for recent news.<br />
     /// Use quotes for exact phrase matching e.g. "climate change"<br />
@@ -37,7 +40,7 @@ public class BraveWebSearchProvider
     /// - Inputs: Query for information.<br />
     /// - Outputs: A list news item summaries from the search engine.
     /// </summary>
-    /// <param name="query">Web search query.</param>
+    /// <param name="query">Web news search query.</param>
     /// <returns></returns>
 #pragma warning disable CS1573 // Parameter has no matching param tag in the XML comment (but other parameters do)
     private async Task<NewsItem[]> NewsSearch(ITool.CallContext callCtx, string query)
@@ -52,17 +55,16 @@ public class BraveWebSearchProvider
         {
             ExtraSnippets = true,
             SafeSearch = dm || nsfw ? SafeSearch.Off : SafeSearch.Moderate,
-            
         });
 
         // Extract results
         return results
-            .Results
-            .Select(NewsItem.Create)
-            .Where(a => a != null)
-            .Take(20)
-            .Cast<NewsItem>()
-            .ToArray();
+              .Results
+              .Select(NewsItem.Create)
+              .Where(a => a != null)
+              .Take(20)
+              .Cast<NewsItem>()
+              .ToArray();
     }
 
     /// <summary>
@@ -100,7 +102,7 @@ public class BraveWebSearchProvider
                 desc,
                 age,
                 source,
-                result.ExtraSnippets ?? [],
+                result.ExtraSnippets ?? [ ],
                 caution
             );
         }
@@ -117,4 +119,90 @@ public class BraveWebSearchProvider
 
         return date > start && date < end;
     }
+    #endregion
+
+    #region web
+    /// <summary>
+    /// General purpose web search.<br />
+    /// Use quotes for exact phrase matching e.g. "climate change"<br />
+    /// Exclude terms with minus e.g. technology -cryptocurrency<br />
+    /// - Capability: General web search.<br />
+    /// - Inputs: Query for information.<br />
+    /// - Outputs: A list results with summaries from the search engine.
+    /// </summary>
+    /// <param name="query">Web search query.</param>
+    /// <param name="maxAgeDays">Maximum age of search results (in days). Specify -1 for no age limit.</param>
+    /// <returns></returns>
+    #pragma warning disable CS1573 // Parameter has no matching param tag in the XML comment (but other parameters do)
+    private async Task<WebItem[]> WebSearch(ITool.CallContext callCtx, string query, int maxAgeDays)
+#pragma warning restore CS1573 // Parameter has no matching param tag in the XML comment (but other parameters do)
+    {
+        // Automatically turn off moderate filtering in DM and NSFW channels
+        var nsfw = callCtx.Channel is IDMChannel or ITextChannel { IsNsfw: true };
+
+        // Age limit
+        var freshness = default(SearchFreshness);
+        if (maxAgeDays >= 0)
+        {
+            var now = DateTime.UtcNow;
+            freshness = SearchFreshness.Range(
+                DateOnly.FromDateTime(now - TimeSpan.FromDays(maxAgeDays)),
+                DateOnly.FromDateTime(now)
+            );
+        }
+        
+        // Run query
+        var results = await _client.SearchAsync(new SearchRequest(query, ResultType.Web)
+        {
+            ExtraSnippets = true,
+            SafeSearch = nsfw ? SafeSearch.Off : SafeSearch.Moderate,
+            Freshness = freshness
+        });
+
+        // Extract results
+        return results
+              .Web?
+              .Results
+              .Select(WebItem.Create)
+              .Where(a => a != null)
+              .Take(20)
+              .Cast<WebItem>()
+              .ToArray() ?? [ ];
+    }
+
+    /// <summary>
+    /// A web item retrieved from a search
+    /// </summary>
+    /// <param name="Title"></param>
+    /// <param name="Description"></param>
+    /// <param name="Age"></param>
+    /// <param name="Nsfw"></param>
+    /// <param name="Source"></param>
+    /// <param name="Snippets"></param>
+    [UsedImplicitly]
+    public record WebItem(string Title, string Description, string Age, bool Nsfw, string Source, IReadOnlyList<string> Snippets)
+    {
+        internal static WebItem? Create(WebResult result)
+        {
+            var desc = result.Description;
+            if (string.IsNullOrWhiteSpace(desc))
+                return null;
+
+            var age = result.Age;
+            if (string.IsNullOrWhiteSpace(age))
+                return null;
+
+            var nsfw = !result.FamilyFriendly;
+            
+            return new WebItem(
+                result.Title,
+                desc,
+                age,
+                nsfw,
+                result.Url,
+                result.ExtraSnippets ?? [ ]
+            );
+        }
+    }
+    #endregion
 }
---
+++ README.md
@@ -0,0 +1,11 @@
+# \*Mute
+
+\*Mute is a Discord bot, intended for my own use in private servers.
+
+## Supporting Packages
+
+- [Autofocus](https://github.com/martindevans/Autofocus): Originally a wrapper for the [AUTOMATIC1111](https://github.com/automatic1111/stable-diffusion-webui) API for image generation. Adapted to also support [Stable Diffusion.cpp](https://github.com/leejet/stable-diffusion.cpp).
+- [BalderHash](https://github.com/martindevans/BalderHash): Generates memorable strings from numbers. e.g. `pagmyr-hapnex` is easier to remember than `284490538`.
+- [Mute.Anilist](https://github.com/martindevans/Mute.Anilist): Wrapper for the [Anilist API](https://docs.anilist.co/).
+- [Mute.BraveSearch](https://github.com/martindevans/Mute.BraveSearch): Wrapper for the [Brave Search API](https://api-dashboard.search.brave.com/documentation/quickstart).
+- [Serpent](https://github.com/martindevans/Serpent): Provides safe sandboxed Python execution within a WASM container.
