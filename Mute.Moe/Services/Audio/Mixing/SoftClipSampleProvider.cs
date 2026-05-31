30e4a1acf6bf9816bbec4166bf7b92f5e03f48da
Martin Evans <martindevans@gmail.com>
2026-05-31 23:44:32 +0100

    Removed native dependency on opus. (#41)
    * Removed native dependency on opus.
     - Concentus (pure C#) for decoding
     - Ported soft clipping
    
    * Removed opus soft clip entirely, replaced with tanh

--- Mute.Moe/Services/Audio/Mixing/SoftClipSampleProvider.cs
+++ Mute.Moe/Services/Audio/Mixing/SoftClipSampleProvider.cs
@@ -1,5 +1,5 @@
-﻿using System.Runtime.InteropServices;
-using NAudio.Wave;
+﻿using NAudio.Wave;
+using System.Numerics.Tensors;
 
 namespace Mute.Moe.Services.Audio.Mixing;
 
@@ -9,8 +9,6 @@ namespace Mute.Moe.Services.Audio.Mixing;
 public partial class SoftClipSampleProvider(ISampleProvider upstream)
     : ISampleProvider
 {
-    private readonly OpusSoftClip _clipper = new(upstream.WaveFormat.Channels);
-
     /// <inheritdoc />
     public WaveFormat WaveFormat => upstream.WaveFormat;
 
@@ -18,53 +16,13 @@ public partial class SoftClipSampleProvider(ISampleProvider upstream)
     public int Read(float[] buffer, int offset, int count)
     {
         var read = upstream.Read(buffer, offset, count);
-        _clipper.Clip(new ArraySegment<float>(buffer, offset, read));
+
+        var span = buffer.AsSpan(offset, read);
+        
+        // y = tanh(1.15 * x)
+        TensorPrimitives.Multiply(span, 1.15f, span);
+        TensorPrimitives.Tanh(span, span);
+
         return read;
-    }
-
-    /// <summary>
-    /// Applies soft clipping to a signal
-    /// </summary>
-    public sealed partial class OpusSoftClip
-    {
-        private readonly float[] _memory;
-
-        /// <summary>
-        /// Create a new soft clip state
-        /// </summary>
-        /// <param name="channels"></param>
-        /// <exception cref="ArgumentOutOfRangeException"></exception>
-        public OpusSoftClip(int channels = 1)
-        {
-            if (channels <= 0)
-                throw new ArgumentOutOfRangeException(nameof(channels), "Channels must be > 0");
-
-            _memory = new float[channels];
-        }
-
-        /// <summary>
-        /// Apply soft clipping to a set of samples
-        /// </summary>
-        /// <param name="samples"></param>
-        public void Clip(ArraySegment<float> samples)
-        {
-            unsafe
-            {
-#if !NCRUNCH
-                using var pin = samples.AsMemory().Pin();
-
-                opus_pcm_soft_clip(
-                    (IntPtr)pin.Pointer,
-                    samples.Count / _memory.Length,
-                    _memory.Length,
-                    _memory
-                );
-#endif
-            }
-        }
-
-        [LibraryImport("opus")]
-        [UnmanagedCallConv(CallConvs = [ typeof(System.Runtime.CompilerServices.CallConvCdecl) ])]
-        private static partial void opus_pcm_soft_clip(nint pcm, int frameSize, int channels, [In, Out] float[] softClipMem);
     }
 }
